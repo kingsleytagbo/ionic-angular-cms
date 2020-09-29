5f2e5a6336fcce211d89539c4d08087a1a7da142
ktagbo <kingsleytagbo@gmail.com>
2020-09-29 08:16:15 -0500

    Toast Messages

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -9,4 +9,15 @@ export class HomePage {
  public loggedIn = false;
   constructor() {}
 
+  public features = [
+    { val: 'Components', isChecked: true },
+    { val: 'Authentication', isChecked: false },
+    { val: 'Data Access', isChecked: false },
+    { val: 'Filtering', isChecked: false },
+    { val: 'Searching', isChecked: false },
+    { val: 'Sorting', isChecked: false },
+    { val: 'User Management', isChecked: false },
+    { val: 'Roles & Permissions Management', isChecked: false },
+  ];
+
 }
--- src/app/providers/UtilityProvider.ts
+++ src/app/providers/UtilityProvider.ts
@@ -7,6 +7,13 @@ export class UtilityProvider{
 
     constructor( private toast: ToastController){}
 
+    /**
+     * A custom reusable toast message
+     * Author: Kingsley Tagbo / https://github.com/kingsleytagbo
+     * @param header: the header / title of your toast message 
+     * @param message: the body or main content of your toast message 
+     * @param overrideOptions: an object with options for customizing your toast, e.g. {position: 'bottom'} 
+     */
     async showToast(header?: string, message?:string, overrideOptions?:any): Promise<HTMLIonToastElement>{
         const toastOptions = {header, message, position:'top', duration: 6000};
         const options = Object.assign(toastOptions, overrideOptions);
