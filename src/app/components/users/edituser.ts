38e43a21bcd23cf88dc10dc667dcaee4e7e4a973
ktagbo <kingsleytagbo@gmail.com>
2020-11-17 03:47:41 -0600

    formating

--- src/app/components/users/edituser.ts
+++ src/app/components/users/edituser.ts
@@ -1,5 +1,8 @@
 import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import User from 'src/app/models/User';
+import { ApiProvider } from '../../services/http';
 
 @Component({
   selector: 'page-edituser',
@@ -9,8 +12,28 @@ import User from 'src/app/models/User';
 export class EditUser{
   @Input('user') user: User;
   isValid = true;
-  constructor( ) { 
+  constructor(     private UsersProvider:ApiProvider,     public router: Router) { 
     this.user = new User('', '');
   }
+  onLogin(form: NgForm) {
+    const username = form.value.username;
+    const password = form.value.password;
+    const user = new User(username, password);
+    const body = {
+      "login": {
+        "username": username,
+        "password": password
+      }
+    };
+    this.UsersProvider.post('/login', body).then((login) =>{
+      console.log({user:user, login:login});
+      if(login.authenticated){
+        this.router.navigateByUrl('/users');
+      }
+    });
+  }
 
+  onSignup() {
+    this.router.navigateByUrl('/signup');
+  }
 }
